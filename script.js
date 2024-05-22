67c6210bd340b1faa129982c231bde55982f6013
abdu <abdu>
2024-05-22 23:07:17 +0500

    Java script

---
+++ script.js
@@ -0,0 +1,45 @@
+var name = prompt("Ismingizni kiriting");
+console.log("Sizning ismingiz: " + name);
+
+var age = +prompt("Yoshingizni kiriting");
+console.log("Sizning yoshingiz: " + age);
+
+var misol1 = +prompt("Misolni yeching : 8+3=?");
+
+if (misol1 >= 11) {
+  console.log("Sizning javob togri 11");
+} else {
+  console.log("Sizning javob noto'g'ri " + misol1 + " Togri javob 11");
+}
+
+var misol2 = +prompt("Misolni yeching : 9-6=?");
+
+if (misol2 >= 3) {
+  console.log("Sizning javob tog'ri 3");
+} else {
+  console.log("Sizning javob noto'g'ri " + misol2 + " Togri javob 3");
+}
+
+var misol3 = +prompt("Misolni yeching : 6*6=?");
+
+if (misol3 >= 36) {
+  console.log("Sizning javob tog'ri 36");
+} else {
+  console.log("Sizning javob noto'g'ri " + misol3 + " Togri javob 36");
+}
+
+var misol4 = +prompt("Misolni yeching : 56/8=?");
+
+if (misol4 >= 7) {
+  console.log("Sizning javob tog'ri 7");
+} else {
+  console.log("Sizning javob noto'gri " + misol4 + " Togri javob 7");
+}
+
+var misol5 = +prompt("Misolni yeching : 29%3=?");
+
+if (misol5 >= 2) {
+  console.log("Sizning javob togri 2");
+} else {
+  console.log("Javob notogri " + misol5 + " Togri javob 2");
+}
